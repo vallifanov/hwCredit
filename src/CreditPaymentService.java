1f2111f3f55af109a30269d2155d8a56b3a4abe6
vallifanov <vallifanov@gmail.com>
2022-03-25 01:23:21 +1200

    игнорирование

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,13 @@
+public class CreditPaymentService {
+    public int calculate( int credit, int years, double percent) {
+        double i = percent / 12 / 100;
+        int n = years * 12;
+        double a = 1.0 + i;
+        Math.pow(a, n);
+        double x = Math.pow(a, n);
+        double y = n - 1.0;
+        double k = (i * x) / (x - 1);
+double s = k * credit;
+return (int) s;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+                CreditPaymentService service = new CreditPaymentService();
+        int resultOneYear = service.calculate(1_000_000, 1, 9.99);
+        System.out.println("Ежемесячный платеж составит: " + resultOneYear);
+        int resultTwoYears = service.calculate(1_000_000, 2, 9.99);
+        System.out.println("Ежемесячный платеж составит: " + resultTwoYears);
+        int resultThreeYears = service.calculate(1_000_000, 3, 9.99);
+        System.out.println("Ежемесячный платеж составит: " + resultThreeYears);
+    }
+}
